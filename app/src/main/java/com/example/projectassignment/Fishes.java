33519ce2732b015e2e3b0bdc802ff73d643e3aee
Novin <a20novra@student.his.se>
2021-05-05 16:21:44 +0200

    Add private member and getter function

--- app/src/main/java/com/example/projectassignment/Fishes.java
+++ app/src/main/java/com/example/projectassignment/Fishes.java
@@ -1,4 +1,44 @@
 package com.example.projectassignment;
 
 public class Fishes {
+    private int id;
+    private String name;
+    private int size;
+    private int cost;
+    private String location;
+
+
+    public Fishes( int id, String name, int size, int cost , String location){
+        this.id = id;
+        this.name = name;
+        this.size = size;
+        this.cost = cost;
+        this.location = location;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public int getCost(){
+        return cost;
+    }
+
+    public String getLocation(){
+        return location;
+    }
+
+    @Override
+    public String toString(){
+        return name;
+    }
 }
+
