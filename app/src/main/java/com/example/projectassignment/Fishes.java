bc1d86bed34af8aa2a9d3d05672fa1f00000ade8
Novin <a20novra@student.his.se>
2021-05-05 16:20:33 +0200

    Add ArrayList && Add ArrayAdapter

---
+++ app/src/main/java/com/example/projectassignment/Fishes.java
@@ -0,0 +1,4 @@
+package com.example.projectassignment;
+
+public class Fishes {
+}
--- app/src/main/java/com/example/projectassignment/MainActivity.java
+++ app/src/main/java/com/example/projectassignment/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.projectassignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<Fishes> item;
+    private ArrayAdapter<Fishes> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
